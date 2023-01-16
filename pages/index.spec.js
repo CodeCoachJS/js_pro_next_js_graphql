821638f0c00ec30938dd81a50c2db8b48f1211d5
Brian Jenney <brianj@Brian-Jenney-MBP16-2019.local>
2023-01-16 09:30:16 -0800

    working tests

--- pages/index.spec.js
+++ pages/index.spec.js
@@ -9,13 +9,55 @@ jest.mock('@apollo/react-hooks', () => ({
 jest.useFakeTimers();
 
 describe('index page', () => {
+  const getUsersMock = jest.fn(); // mock the getUsers function and be able to inspect it
+
   it('should render', async () => {
     // TODO: add tests when the page is loading
+    useLazyQuery.mockImplementation(() => [
+      getUsersMock,
+      {
+        loading: true,
+        data: null
+      }
+    ]);
+
+    const { rerender } = render(<IndexPage />);
     expect(screen.getByText('Loading...')).toBeInTheDocument();
 
+    useLazyQuery.mockImplementation(() => [
+      getUsersMock,
+      {
+        loading: false,
+        data: {
+          user: {
+            avatarUrl: 'https://avatars.githubusercontent.com/u/1?v=4',
+            repositories: { totalCount: 5 }
+          }
+        }
+      }
+    ]);
+
+    rerender(<IndexPage />);
     // TODO: add tests for a user with 5 respositories
-    expect(screen.getByText('Repository Count: 5')).toBeInTheDocument();
+    expect(
+      screen.getByText('Repository Count: 5', { exact: false })
+    ).toBeInTheDocument();
     expect(screen.getByTestId('user-avatar')).toBeInTheDocument();
+
+    useLazyQuery.mockImplementation(() => [
+      getUsersMock,
+      {
+        loading: false,
+        data: {
+          user: {
+            avatarUrl: 'https://avatars.githubusercontent.com/u/1?v=4',
+            repositories: { totalCount: 4 }
+          }
+        }
+      }
+    ]);
+
+    rerender(<IndexPage />);
 
     // TODO: add tests for a user with 4 respositories
     expect(screen.getByText('Repository Count: 4')).toBeInTheDocument();
@@ -38,6 +80,23 @@ describe('index page', () => {
 
     // TODO: test the the debounced function is called after 1 second
 
-    expect(getUsersMock).toHaveBeenCalledTimes(2);
+    const input = screen.getByTestId('search-input');
+    // it only calls the function once every 1000ms
+
+    fireEvent.change(input, { target: { value: '123' } });
+    jest.advanceTimersByTime(250);
+    fireEvent.change(input, { target: { value: '456' } });
+    jest.advanceTimersByTime(250); // 500ms
+    fireEvent.change(input, { target: { value: '123' } });
+    jest.advanceTimersByTime(250); // 750ms
+    fireEvent.change(input, { target: { value: '456' } });
+    jest.advanceTimersByTime(250); // 1000ms
+    fireEvent.change(input, { target: { value: '123' } });
+    jest.advanceTimersByTime(250); // 1250ms
+    fireEvent.change(input, { target: { value: '456' } });
+    jest.advanceTimersByTime(250); // 1500ms
+    // TODO: test the the debounced function is called after 1 second
+
+    expect(getUsersMock).toHaveBeenCalledTimes(3);
   });
 });
